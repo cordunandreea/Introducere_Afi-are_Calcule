f9645a79a8255dcc896dfa6b5bcaa59a54eeba1f
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-09-29 22:46:29 +0300

    8
    . Se introduc de la tastatură trei cifre. Afişaţi pe aceeaşi linie 5 numere formate cu
    aceste cifre luate o singură dată. Exemplu : date de intrare : 3 4 2 Date de ieşire : 324
    342 243 234 432.

---
+++ problema8.py
@@ -0,0 +1,10 @@
+print('Introduceti 3 cifre aleatorii')
+n1=int(input('n1='))
+n2=int(input('n2='))
+n3=int(input('n3='))
+I=n1*100+n2*10+n3
+II=n1*100+n3*10+n2
+III=n2*100+n1*10+n3
+IV=n2*100+n3*10+n1
+V=n3*100+n1*10+n2
+print(I,II,III,IV,V)
