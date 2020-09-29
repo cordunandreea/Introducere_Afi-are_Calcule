a424d48a934721a27f6ea0c37d32da15b1ff9e70
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-09-29 22:42:39 +0300

    1
    De la tastatură se întroduce numărul de rînd al culorii curcubeului. De afişat
    denumirea culorii. Convenim că culoarea roşie are numărul de rînd 1.

---
+++ problema1.py
@@ -0,0 +1,16 @@
+n=int(input("Introduceti numarul culorii="))
+print("Ati introdus culoarea cu numarul:", n)
+if n=1;
+print("Rosu")
+if n=2;
+print("Oranj")
+if n=3;
+print("Galben")
+if n=4;
+print("Verde")
+if n=5;
+print("Albastru")
+if n=6;
+print("Indigo")
+if n=7;
+print("Violet")
