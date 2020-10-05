5f1cc1994f16ef5ac15422127985f99bf8bda024
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-10-05 17:45:44 +0300

    10
    .Afişaţi tabla înmulţirii cu numărul n. Exemplu: pentru n=5, se va afişa pe verticală
    1x5=5 2x5=10 3x5=15 4x5=20 5x5=25 6x5=30 7x5=35 8x5=40 9x5=45 10x5=50

---
+++ problema10.py
@@ -0,0 +1,22 @@
+print('dati valoare lui n:')
+n=int(input('n='))
+produsul=1*n
+produsul2=2*n
+produsul3=3*n
+produsul4=4*n
+produsul5=5*n
+produsul6=6*n
+produsul7=7*n
+produsul8=8*n
+produsul9=9*n
+produsul10=10*n
+print('1*n=',produsul)
+print('2*n=',produsul2)
+print('3*n=',produsul3)
+print('4*n=',produsul4)
+print('5*n=',produsul5)
+print('6*n=',produsul6)
+print('7*n=',produsul7)
+print('8*n=',produsul8)
+print('9*n=',produsul9)
+print('10*n=',produsul10)
