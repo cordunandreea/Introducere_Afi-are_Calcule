11a9d5393deef4509add1b500471cb1f647755fa
cordunandreea <70647953+cordunandreea@users.noreply.github.com>
2020-10-05 17:58:35 +0300

    11
    .Măriuca ţine evidenţa iepurilor din crescătorie. Ea îşi notează câţi iepuri sunt la
    începutul fiecărei luni, câţi au murit şi câţi s-au născut în cursul fiecăei luni. Puteţi să
    realizaţi un program care, primind aceste date, să afişeze la sfârşitul fiecărei luni câţi
    iepuri sunt în crescătorie? Exemplu : Date de intrare : nr. Iepuri la început de luna 10
    nr. iepuri morti 2 nr. iepuri nascuti 6 Date de ieşire : 14 iepuri.

---
+++ Problema_11.py
@@ -0,0 +1,5 @@
+iil=int(input('nr de iepuri la inceputul lunii:'))
+im=int(input('nr de iepuri morti:'))
+inn=int(input('nr de iepuri nascuti:'))
+it=iil-im+inn
+print('nr total de iepuri la sfarsitul lunii este:',it, 'iepuri')
